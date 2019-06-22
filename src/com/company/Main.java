6d439935ddaf66cd641a98fa095526f666dab81f
big <big_sethawit@hotmail.com>
2019-06-22 21:10:28 +0700

    1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,170 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
+class BankingThread extends Thread implements Comparable<BankingThread> {
+    private Account account;
+    private String mode;
+    private int totalbalance;
+    private boolean loop = true;
+    public BankingThread(String n, Account ac, String m)
+    {
+        super(n);
+        account = ac;
+        mode = m;
+    }
+    public void run()
+    {
+    /* Add a loop to process either 5 consecutive deposits (mode = m) or 5
+    consecutive withdraws (mode = w). For each transaction processing
+    1. Random the amount of money to deposit/withdraw. You may set the range of
+    random values, e.g. 1-10
+    2. Call account.deposit(…) or account.withdraw(…) to deposit/withdraw money
+    */
+        int count_loop = 1;
+        while (loop)
+        {
+            Random rand = new Random();
+            int  money = rand.nextInt(10) + 1;
+            try
+            {
+                synchronized (this) {
+                    switch (mode) {
+                        case "d":
+                        case "D":
+                            account.deposit(money, this.getName(), count_loop);
+                            totalbalance += money;
+                            Thread.sleep(5);
+                            break;
+                        case "w":
+                        case "W":
+                            account.withdraw(money, this.getName(), count_loop);
+                            totalbalance -= money;
+                            Thread.sleep(5);
+                            break;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                System.err.println(e);
+            }
+            count_loop++;
+            if (count_loop == 6)
+            {
+                loop = false;
+            }
+        }
+    }
+
+    @Override
+    public int compareTo(BankingThread t) {
+        if (this.totalbalance < t.totalbalance) {
+            return -1;
+        } else if (this.totalbalance > t.totalbalance) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+} // end BankingThread
+
+class Account {
+    private int balance;
+    public Account(int b) {
+        balance = b;
+    }
+    synchronized public void deposit(int money, String name, int count)
+    {
+        // Update balance &amp; print thread name, transaction ID, money to deposit, balance
+        balance += money;
+        System.out.printf("%s  transaction %d  + %-2d  balance = %-3d\n", name, count, money, balance);
+    }
+    public synchronized boolean withdraw(int money, String name, int count)
+    {
+    /* If there is enough money to withdraw
+    - Update balance
+    - Print thread name, transaction ID, money to withdraw, balance
+    Otherwise
+    - Print thread name, transaction ID, money to withdraw, failure message
+    */
+        if (balance < money)
+        {
+            System.out.printf("%s  transaction %d  - %-2d  fails\n", name, count, money);
+            return false;
+        }
+        else if (balance == money)
+        {
+            balance -= money;
+            System.out.printf("%s  transaction %d  - %-2d  balance = %-3d\n", name, count, money, balance);
+            return true;
+        }
+        else
+        {
+            balance -= money;
+            System.out.printf("%s  transaction %d  - %-2d  balance = %-3d\n", name, count, money, balance);
+            return true;
+        }
+    }
+    public int print(){
+        System.out.printf("===== Final Balance = %d =====\n", balance);
+        System.out.println();
+        return balance;
+    }
+} // end Account
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        boolean doTrans = true;
+        boolean flag = true;
+        int bal = 0;
+        Scanner scan = new Scanner(System.in);
+
+        System.out.printf("Initial Balance   = ");
+        int initial = scan.nextInt();
+
+        System.out.printf("Number of threads = ");
+        int numThread = scan.nextInt();
+        System.out.println();
+
+        while(doTrans){
+
+            System.out.printf("Enter D (deposit), W (withdraw), or other (quit) = ");
+            String md = scan.next().trim();
+
+            if(md.equals("d")||md.equals("D")||md.equals("w")||md.equals("W")) {
+                doTrans = true;
+            } else break;
+
+            if(!flag)
+                initial = bal;
+
+            ArrayList<BankingThread> list = new ArrayList<>();
+            Account ac = new Account(initial);
+
+            for (int i = 0; i < numThread; i++) {
+                list.add(new BankingThread("T"+(i+1),ac,md));
+            }
+
+            for (int i = 0; i < numThread; i++) {
+                list.get(i).start();
+            }
+
+            try {
+                for (int i = 0; i < numThread; i++) {
+                    list.get(i).join();
+                }
+            }
+
+            catch (InterruptedException e) {
+            }
+
+            bal = ac.print();
+            flag = false;
+        }
     }
 }
