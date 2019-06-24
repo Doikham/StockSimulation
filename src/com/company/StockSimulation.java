847e078d82570794055d20028e01f87ba8393606
big <big_sethawit@hotmail.com>
2019-06-24 23:00:31 +0700

    up 24/06/19

--- src/com/company/StockSimulation.java
+++ src/com/company/StockSimulation.java
@@ -6,188 +6,55 @@ import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
 
-//class BankingThread extends Thread implements Comparable<BankingThread> {
-//    private Account account;
-//    private String mode;
-//    private int totalbalance;
-//    private boolean loop = true;
-//    public BankingThread(String n, Account ac, String m)
-//    {
-//        super(n);
-//        account = ac;
-//        mode = m;
-//    }
-//    public void run()
-//    {
-//    /* Add a loop to process either 5 consecutive deposits (mode = m) or 5
-//    consecutive withdraws (mode = w). For each transaction processing
-//    1. Random the amount of money to deposit/withdraw. You may set the range of
-//    random values, e.g. 1-10
-//    2. Call account.deposit(…) or account.withdraw(…) to deposit/withdraw money
-//    */
-//        int count_loop = 1;
-//        while (loop)
-//        {
-//            Random rand = new Random();
-//            int  money = rand.nextInt(10) + 1;
-//            try
-//            {
-//                synchronized (this) {
-//                    switch (mode) {
-//                        case "d":
-//                        case "D":
-//                            account.deposit(money, this.getName(), count_loop);
-//                            totalbalance += money;
-//                            Thread.sleep(5);
-//                            break;
-//                        case "w":
-//                        case "W":
-//                            account.withdraw(money, this.getName(), count_loop);
-//                            totalbalance -= money;
-//                            Thread.sleep(5);
-//                            break;
-//                    }
-//                }
-//            }
-//            catch (Exception e)
-//            {
-//                System.err.println(e);
-//            }
-//            count_loop++;
-//            if (count_loop == 6)
-//            {
-//                loop = false;
-//            }
-//        }
-//    }
-//
-//    @Override
-//    public int compareTo(BankingThread t) {
-//        if (this.totalbalance < t.totalbalance) {
-//            return -1;
-//        } else if (this.totalbalance > t.totalbalance) {
-//            return 1;
-//        } else {
-//            return 0;
-//        }
-//    }
-//
-//} // end BankingThread
-
-//class Account {
-////    private int balance;
-////    public Account(int b) {
-////        balance = b;
-////    }
-////    synchronized public void deposit(int money, String name, int count)
-////    {
-////        // Update balance &amp; print thread name, transaction ID, money to deposit, balance
-////        balance += money;
-////        System.out.printf("%s  transaction %d  + %-2d  balance = %-3d\n", name, count, money, balance);
-////    }
-////    public synchronized boolean withdraw(int money, String name, int count)
-////    {
-////    /* If there is enough money to withdraw
-////    - Update balance
-////    - Print thread name, transaction ID, money to withdraw, balance
-////    Otherwise
-////    - Print thread name, transaction ID, money to withdraw, failure message
-////    */
-////        if (balance < money)
-////        {
-////            System.out.printf("%s  transaction %d  - %-2d  fails\n", name, count, money);
-////            return false;
-////        }
-////        else if (balance == money)
-////        {
-////            balance -= money;
-////            System.out.printf("%s  transaction %d  - %-2d  balance = %-3d\n", name, count, money, balance);
-////            return true;
-////        }
-////        else
-////        {
-////            balance -= money;
-////            System.out.printf("%s  transaction %d  - %-2d  balance = %-3d\n", name, count, money, balance);
-////            return true;
-////        }
-////    }
-////    public int print(){
-////        System.out.printf("===== Final Balance = %d =====\n", balance);
-////        System.out.println();
-////        return balance;
-////    }
-////} // end Account
-
-//public class StockSimulation {
-//
-//    public static void main(String[] args) {
-//
-//        boolean doTrans = true;
-//        boolean flag = true;
-//        int bal = 0;
-//        Scanner scan = new Scanner(System.in);
-//
-//        System.out.printf("Initial Balance   = ");
-//        int initial = scan.nextInt();
-//
-//        System.out.printf("Number of threads = ");
-//        int numThread = scan.nextInt();
-//        System.out.println();
-//
-//        while(doTrans){
-//
-//            System.out.printf("Enter D (deposit), W (withdraw), or other (quit) = ");
-//            String md = scan.next().trim();
-//
-//            if(md.equals("d")||md.equals("D")||md.equals("w")||md.equals("W")) {
-//                doTrans = true;
-//            } else break;
-//
-//            if(!flag)
-//                initial = bal;
-//
-//            ArrayList<BankingThread> list = new ArrayList<>();
-//            Account ac = new Account(initial);
-//
-//            for (int i = 0; i < numThread; i++) {
-//                list.add(new BankingThread("T"+(i+1),ac,md));
-//            }
-//
-//            for (int i = 0; i < numThread; i++) {
-//                list.get(i).start();
-//            }
-//
-//            try {
-//                for (int i = 0; i < numThread; i++) {
-//                    list.get(i).join();
-//                }
-//            }
-//
-//            catch (InterruptedException e) {
-//            }
-//
-//            bal = ac.print();
-//            flag = false;
-//        }
-//    }
-//}
-
 class Product {
     private String name;
     private int balance;
     public Product (String n){
         name = n;
     }
-    public void addToStock(){
-
+    public Product (int g){
+        balance = g;
     }
-    public void removeFromStock (){
+    synchronized public void addToStock(String nme, int value, int n){
+        balance += value;
+        System.out.printf("%s > trans   %-3d   +%d  %-20s  balance =     " +
+                "%-5d", Thread.currentThread().getName(), n, value, nme, balance);
+    }
+    synchronized public void removeFromStock (){
 
     }
 }
 
 class VendorThread extends Thread{
+    private String productName;
+    private int numTransaction;
+    private int productValue;
+    private Product product;
+    private int total;
+    public VendorThread(int a, String b, int c, Product d){
+        numTransaction = a;
+        productName = b;
+        productValue = c;
+        product = d;
+    }
+    public void run(){
+        ArrayList <Product> allStocks = new ArrayList <> ();
+        try{
+            if (productValue > 0){
+                synchronized (this){
+                    product.addToStock(this.getName(),productValue,numTransaction);
+                    //allStocks.add(this.productName,productValue);
+                    total += productValue;
+                    Thread.sleep(5);
+                }
+            } else {
 
+            }
+        }
+        catch (Exception e){
+            System.out.println(e);
+        }
+    }
 }
 
 public class StockSimulation {
@@ -195,8 +62,11 @@ public class StockSimulation {
     public static void main(String[] args) {
 
         int SimTime = 1;
+        int initial = 0;
 
         ArrayList <Product> product = new ArrayList <> ();
+        ArrayList <VendorThread>  trans = new ArrayList <> ();
+        Product pr = new Product(initial);
 
         while(true){
             System.out.printf("%s > Enter product file = ", Thread.currentThread().getName());
@@ -205,8 +75,8 @@ public class StockSimulation {
             try{
                 Scanner scan = new Scanner(new File(proFile));
                 while(scan.hasNextLine()){
-                    String nm = scan.nextLine();
-                    product.add(new Product(nm));
+                    String nam = scan.nextLine();
+                    product.add(new Product(nam));
                 }
                 break;
             } catch (FileNotFoundException e) {
@@ -216,13 +86,22 @@ public class StockSimulation {
 
         while(true){
             System.out.printf("%s > Enter transaction file for vendor 1 = ", Thread.currentThread().getName());
-            Scanner trans1 = new Scanner(System.in);
-            String file1 = trans1.next();
+            Scanner in1 = new Scanner(System.in);
+            String file1 = in1.next();
             try{
                 Scanner scan = new Scanner(new File(file1));
                 while(scan.hasNextLine()){
-                    String nm = scan.nextLine();
-                    product.add(new Product(nm));
+                    String line = scan.nextLine();
+                    String [] buffer = line.split(",");
+                    try{
+                        int numTrans = Integer.parseInt(buffer[0].trim());
+                        String nm = buffer[1].trim();
+                        int value = Integer.parseInt(buffer[2].trim());
+                        trans.add(new VendorThread(numTrans,nm,value,pr));
+                    }
+                    catch (RuntimeException e){
+                        System.out.println(e);
+                    }
                 }
                 break;
             } catch (FileNotFoundException e) {
@@ -232,13 +111,22 @@ public class StockSimulation {
 
         while(true){
             System.out.printf("%s > Enter transaction file for vendor 2 = ", Thread.currentThread().getName());
-            Scanner trans2 = new Scanner(System.in);
-            String file2 = trans2.next();
+            Scanner in2 = new Scanner(System.in);
+            String file2 = in2.next();
             try{
                 Scanner scan = new Scanner(new File(file2));
                 while(scan.hasNextLine()){
-                    String nm = scan.nextLine();
-                    product.add(new Product(nm));
+                    String line = scan.nextLine();
+                    String [] buffer = line.split(",");
+                    try{
+                        int numTrans = Integer.parseInt(buffer[0].trim());
+                        String nm = buffer[1].trim();
+                        int value = Integer.parseInt(buffer[2].trim());
+                        trans.add(new VendorThread(numTrans,nm,value,pr));
+                    }
+                    catch (RuntimeException e){
+                        System.out.println(e);
+                    }
                 }
                 break;
             } catch (FileNotFoundException e) {
@@ -248,13 +136,22 @@ public class StockSimulation {
 
         while(true){
             System.out.printf("%s > Enter transaction file for vendor 3 = ", Thread.currentThread().getName());
-            Scanner trans3 = new Scanner(System.in);
-            String file3 = trans3.next();
+            Scanner in3 = new Scanner(System.in);
+            String file3 = in3.next();
             try{
                 Scanner scan = new Scanner(new File(file3));
                 while(scan.hasNextLine()){
-                    String nm = scan.nextLine();
-                    product.add(new Product(nm));
+                    String line = scan.nextLine();
+                    String [] buffer = line.split(",");
+                    try{
+                        int numTrans = Integer.parseInt(buffer[0].trim());
+                        String nm = buffer[1].trim();
+                        int value = Integer.parseInt(buffer[2].trim());
+                        trans.add(new VendorThread(numTrans,nm,value,pr));
+                    }
+                    catch (RuntimeException e){
+                        System.out.println(e);
+                    }
                 }
                 break;
             } catch (FileNotFoundException e) {
@@ -265,5 +162,19 @@ public class StockSimulation {
         System.out.printf("%s > ------------------------------\n", Thread.currentThread().getName());
         System.out.printf("%s >      %S (%d)\n", Thread.currentThread().getName(), "Stock Simulation", SimTime);
         System.out.printf("%s > ------------------------------\n", Thread.currentThread().getName());
+
+        for (int i = 0; i < 3; i++) {
+            trans.get(i).start();
+        }
+
+        try {
+            for (int i = 0; i < 3; i++) {
+                trans.get(i).join();
+            }
+        }
+
+        catch (InterruptedException e) {
+
+        }
     }
 }
